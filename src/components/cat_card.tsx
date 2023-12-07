9988627137b7ceee3886e04fa2d0fb08d103713e
judejay <judejay26@gmail.com>
2023-12-07 21:30:43 +0000

    refactor: destructuring props

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -4,12 +4,14 @@ interface CatCardProps {
   catObject: Cat;
 }
 
-const CatCard: React.FC<CatCardProps> = (props) => (
+const CatCard: React.FC<CatCardProps> = ({
+  catObject: { name, species, favFoods, birthYear },
+}) => (
   <div className="card">
-    <h3 className="card__text card__header">{props.catObject.name}</h3>
-    <p className="card__text">Species: {props.catObject.species}</p>
-    <p className="card__text">Favourite Food(s): {props.catObject.favFoods}</p>
-    <p className="card__text">Birth Year: {props.catObject.birthYear}</p>
+    <h3 className="card__text card__header">{name}</h3>
+    <p className="card__text">Species: {species}</p>
+    <p className="card__text">Favourite Food(s): {favFoods}</p>
+    <p className="card__text">Birth Year: {birthYear}</p>
   </div>
 );
 
