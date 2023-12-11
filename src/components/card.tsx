d34d5571c2b13d7bd66860d3af0aa4ccacf71dca
judejay <judejay26@gmail.com>
2023-12-11 14:40:25 +0000

    Feat: comma separated list from array of foods

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -26,7 +26,7 @@ const Card: React.FC<CardProps> = ({
   <div className="card">
     <h3 className="card__text card__header">{name}</h3>
     <p className="card__text">Species: {species}</p>
-    <p className="card__text">Favourite Food(s): {favFoods}</p>
+    <p className="card__text">Favourite Food(s): {favFoods.toString()}</p>
     <p className="card__text">Birth Year: {birthYear}</p>
     {
       <Image
